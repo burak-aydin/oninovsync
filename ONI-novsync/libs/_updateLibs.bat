d7db758264cedf9ec37165ab57177a3f51dc99b5
hooap <45029830+hooap@users.noreply.github.com>
2020-02-13 17:35:35 +0300

    Delete _updateLibs.bat

--- ONI-novsync/libs/_updateLibs.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set PATH=%ONI_PATH%\OxygenNotIncluded_Data\Managed
-
-xcopy "%PATH%\Unity*.dll" %~dp0 /Y
-xcopy "%PATH%\Assembly-CSharp*.dll" %~dp0 /Y
-xcopy "%PATH%\0Harmony.dll" %~dp0 /Y
